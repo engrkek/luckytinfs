188fddc86cdb80cec305ea910bfccb3cf95dd24b
Larra Su <larramsu@gmail.com>
2026-07-15 01:24:17 -0700

    feat: add padding

--- app/components/Stamp.vue
+++ app/components/Stamp.vue
@@ -10,20 +10,30 @@ const props = withDefaults(defineProps<{
 
 <template>
   <div class="stamp overflow-hidden" :class="props.class">
-    <NuxtImg :src :alt draggable="false" class="w-full h-full object-cover object-center" />
+    <div class="stamp__matte">
+      <NuxtImg :src :alt draggable="false" class="w-full h-full object-cover object-center" />
+    </div>
   </div>
 </template>
 
 <style lang="css" scoped>
 .stamp {
   --r: 10px;
+  --pad: 4px;
   width: 100%;
-  aspect-ratio: 1.5;
   padding: var(--r);
   background: #fff;
   mask:
-    radial-gradient(at 50% 50%,#0000 50%,#000 50%) round
-    var(--r) var(--r)/calc(2*var(--r)) calc(2*var(--r)),
+    radial-gradient(at 50% 50%, #0000 50%, #000 50%) round
+      var(--r) var(--r) / calc(2 * var(--r)) calc(2 * var(--r)),
     conic-gradient(#000 0 0) content-box;
+}
+
+.stamp__matte {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  padding: var(--pad);
+  background: #fff;
 }
 </style>
