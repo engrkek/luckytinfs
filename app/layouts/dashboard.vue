3c78be8f7f9e19a056818d4a8c1674726eef09b1
Larra Su <larramsu@gmail.com>
2026-07-16 03:35:45 -0700

    feat: add initial office

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -1,8 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  meta: [
+    { key: 'theme-color', name: 'theme-color', content: '#1f4072' },
+  ],
+  htmlAttrs: {
+    class: 'scroll-smooth',
+  },
+  bodyAttrs: {
+    class: 'overflow-x-hidden bg-secondary-950 text-primary-100 selection:bg-primary-300 selection:text-secondary-950',
+  },
+})
+
+const tabs = [
+  { icon: 'ph:star-four-fill', label: 'Home', to: '/office' },
+  { icon: 'ph:folder', label: 'Projects', to: '/office/projects' },
+  { icon: 'ph:heart', label: 'Donations', to: '/office/donations' },
+  { icon: 'ph:receipt', label: 'Expenses', to: '/office/expenses' },
+]
+</script>
 
 <template>
-  <div>
-    Layout: dashboard
-    <slot />
+  <div class="w-full h-dvh flex flex-col">
+    <div class="flex-1 overflow-x-hidden px-4 py-3">
+      <slot />
+    </div>
+
+    <div class="bg-secondary-950 text-primary-100 border-t border-secondary-900 flex items-center justify-around px-4 pt-3 pb-4">
+      <NuxtLink
+        v-for="(tab, index) in tabs"
+        :key="index"
+        :to="tab.to"
+        class="flex flex-col items-center gap-0.5"
+        active-class="text-primary-300"
+      >
+        <UIcon :name="tab.icon" />
+        <p class="text-xs">{{ tab.label }}</p>
+      </NuxtLink>
+    </div>
   </div>
 </template>
--- app/pages/office/index.vue
+++ app/pages/office/index.vue
@@ -2,6 +2,16 @@
 
 <template>
   <div>
-    Page: office/index
+    <div>
+      <h1 class="font-display text-4xl tracking-tighter">
+        welcome back
+      </h1>
+    </div>
+
+    <!-- Stats -->
+
+    <!-- Needs attention -->
+
+    <!-- Recent activities -->
   </div>
 </template>
